2a463caf3311c66f9b5fdfd3a7dd133a8470827f
Luke <luklg@outlook.com>
2026-01-19 22:46:26 -0300

    Service Layer e Component Registration

---
+++ src/main/java/com/lukegl/webservice/services/UserService.java
@@ -0,0 +1,25 @@
+package com.lukegl.webservice.services;
+
+import com.lukegl.webservice.entities.User;
+import com.lukegl.webservice.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long Id){
+        Optional<User> obj = repository.findById(Id);
+        return obj.get();
+    }
+}
